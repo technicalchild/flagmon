8559ef7f1a5613cbbc50994c3ee0bdbe4978ca63
anon <anon@anon.com>
2017-04-04 11:25:02 -0500

    add special user

--- special.py
+++ special.py
@@ -3,6 +3,6 @@ from models import User, Flag
 
 # profile fetch fails with certain special characters, even with URL-encoding
 def load_special_users():
-    session.merge(User(userid=62448, username="ACRE & EQUAT"))
+    session.merge(User(userid=62448, username="ACRE &amp; EQUAT"))
     session.merge(User(userid=94203, username="+2 Sword of Chutney"))
     session.commit()
